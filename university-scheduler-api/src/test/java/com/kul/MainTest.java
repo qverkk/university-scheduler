d5885f82d4933d5c617c78b7da6e596503626ea5
qverkk <membersy@gmail.com>
2020-03-06 19:57:46 +0100

    Add initial unit tests for database and api modules

---
+++ university-scheduler-api/src/test/java/com/kul/MainTest.java
@@ -0,0 +1,14 @@
+package com.kul;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void test() {
+        assertEquals(3, 1 + 2);
+    }
+
+}
--- university-scheduler-database/src/test/java/com/kul/universityschedulerdatabase/UniversitySchedulerDatabaseApplicationTests.java
+++ university-scheduler-database/src/test/java/com/kul/universityschedulerdatabase/UniversitySchedulerDatabaseApplicationTests.java
@@ -3,11 +3,14 @@ package com.kul.universityschedulerdatabase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class UniversitySchedulerDatabaseApplicationTests {
 
     @Test
     void contextLoads() {
+        assertEquals(3, 1 + 2);
     }
 
 }
