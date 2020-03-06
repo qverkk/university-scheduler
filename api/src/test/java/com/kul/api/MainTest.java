020948500d9b35023c85ab73fbd06d71185de6d4
qverkk <membersy@gmail.com>
2020-03-06 19:25:15 +0100

    Add junit 5 and an example test

---
+++ api/src/test/java/com/kul/api/MainTest.java
@@ -0,0 +1,14 @@
+package com.kul.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    public void test() {
+        assertEquals(3, 1 + 2);
+    }
+
+}
