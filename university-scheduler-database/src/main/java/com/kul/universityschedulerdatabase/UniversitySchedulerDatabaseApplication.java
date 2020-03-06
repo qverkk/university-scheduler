0245847d2dce67bc6cc2c9f7aa51727152728301
qverkk <membersy@gmail.com>
2020-03-06 19:54:20 +0100

    Initialized api and database modules

---
+++ university-scheduler-database/src/main/java/com/kul/universityschedulerdatabase/UniversitySchedulerDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.kul.universityschedulerdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySchedulerDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitySchedulerDatabaseApplication.class, args);
+    }
+
+}
