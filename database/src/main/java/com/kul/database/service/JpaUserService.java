c93f421f584b89908e05c3c61d426e65591043df
qverkk <membersy@gmail.com>
2020-03-28 18:49:52 +0100

    feat: Add authentication code

--- database/src/main/java/com/kul/database/service/JpaUserService.java
+++ database/src/main/java/com/kul/database/service/JpaUserService.java
@@ -1,9 +1,14 @@
 package com.kul.database.service;
 
+import com.kul.database.constants.JwtUtils;
 import com.kul.database.model.Authority;
 import com.kul.database.model.User;
 import com.kul.database.repository.AuthorityRepository;
 import com.kul.database.repository.UserRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -13,16 +18,30 @@ public class JpaUserService implements UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final AuthorityRepository authorityRepository;
+    private final AuthenticationManager authenticationManager;
 
-    public JpaUserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthorityRepository authorityRepository) {
+    public JpaUserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthorityRepository authorityRepository, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.authorityRepository = authorityRepository;
+        this.authenticationManager = authenticationManager;
     }
 
     @Override
     public String authenticate(User user) {
-        return null;
+        User repositoryUser = userRepository.findByUsername(user.getUsername());
+        if (repositoryUser == null) {
+            return null;
+        }
+        if (!passwordEncoder.matches(user.getPassword(), repositoryUser.getPassword())) {
+            return null;
+        }
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = JwtUtils.generateToken(authentication);
+        return token;
     }
 
     @Override
