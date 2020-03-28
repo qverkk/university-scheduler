7294c60cbd33ccb1906f7a2d2e6f58adb48b00e4
qverkk <membersy@gmail.com>
2020-03-28 18:45:24 +0100

    feat: Add registration code for user

--- database/src/main/java/com/kul/database/service/JpaUserService.java
+++ database/src/main/java/com/kul/database/service/JpaUserService.java
@@ -1,16 +1,23 @@
 package com.kul.database.service;
 
+import com.kul.database.model.Authority;
 import com.kul.database.model.User;
+import com.kul.database.repository.AuthorityRepository;
 import com.kul.database.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("User service")
 public class JpaUserService implements UserService {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthorityRepository authorityRepository;
 
-    public JpaUserService(UserRepository userRepository) {
+    public JpaUserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthorityRepository authorityRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.authorityRepository = authorityRepository;
     }
 
     @Override
@@ -25,6 +32,16 @@ public class JpaUserService implements UserService {
 
     @Override
     public Boolean registerUser(User user) {
-        return null;
+        if (userRepository.findByUsername(user.getUsername()) != null) {
+            return false;
+        }
+        user.setId(null);
+        user.setEnabled(false);
+        user.setUsername(passwordEncoder.encode(user.getPassword()));
+        if (authorityRepository.findByAuthority(user.getAuthority().getAuthority()) == null) {
+            authorityRepository.save(new Authority(null, user.getAuthority().getAuthority()));
+        }
+        userRepository.save(user);
+        return true;
     }
 }
