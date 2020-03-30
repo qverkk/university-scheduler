6b802d12c61957e38462b0ae305bc55300faa32b
Michał <michal.przepiorka0@gmail.com>
2020-03-30 22:48:56 +0200

    Add login and registration UI

--- window/src/main/java/com/kul/window/Main.java
+++ window/src/main/java/com/kul/window/Main.java
@@ -1,4 +1,22 @@
 package com.kul.window;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("DocumentRegistrationForm.fxml"));
+        primaryStage.setTitle("Register panel");
+        primaryStage.setScene(new Scene(root, 700, 400));
+        primaryStage.show();
+    }
 }
---
+++ window/src/main/java/com/kul/window/login/LoginController.java
@@ -0,0 +1,19 @@
+package com.kul.window.login;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Y510p
+ * @project university-scheduler
+ * @date 30.03.2020
+ **/
+
+public class LoginController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
