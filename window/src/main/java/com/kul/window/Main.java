7d3c8a6081b23b4e2ce4b61a55ffe23cd72f4502
qverkk <membersy@gmail.com>
2020-03-31 00:20:34 +0200

    feat: Move fxmls and images to resource package and fix loading the views and images

--- window/src/main/java/com/kul/window/Main.java
+++ window/src/main/java/com/kul/window/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("DocumentRegistrationForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/com/kul/window/panes/DocumentLoginForm.fxml"));
         primaryStage.setTitle("Register panel");
         primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
