9da940e38d7bc8a397620168a06b3374e2cf8f7d
qverkk <membersy@gmail.com>
2020-05-02 12:26:16 +0200

    fix: Propper mapping LocalTime to String with usage of DateTimeFormatter

--- database/src/main/java/com/kul/database/lecturerpreferences/adapter/FetchLecturerPreferenceMapper.java
+++ database/src/main/java/com/kul/database/lecturerpreferences/adapter/FetchLecturerPreferenceMapper.java
@@ -4,16 +4,18 @@ import com.kul.database.lecturerpreferences.api.FetchLecturerPreferenceResponse;
 import com.kul.database.lecturerpreferences.domain.LecturerPreferences;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class FetchLecturerPreferenceMapper {
     public static FetchLecturerPreferenceResponse toResponse(LecturerPreferences lecturerPreferences) {
         return new FetchLecturerPreferenceResponse(
-                localTimeToHourAndMinute(lecturerPreferences.getStartTime()),
-                localTimeToHourAndMinute(lecturerPreferences.getEndTime())
+                localTimeToString(lecturerPreferences.getStartTime()),
+                localTimeToString(lecturerPreferences.getEndTime())
         );
     }
 
-    private static String localTimeToHourAndMinute(LocalTime time) {
-        return time.getHour() + ":" + time.getMinute();
+    private static String localTimeToString(LocalTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return formatter.format(time);
     }
 }
--- database/src/main/java/com/kul/database/lecturerpreferences/adapter/UpdateLecturerPreferenceMapper.java
+++ database/src/main/java/com/kul/database/lecturerpreferences/adapter/UpdateLecturerPreferenceMapper.java
@@ -4,19 +4,21 @@ import com.kul.database.lecturerpreferences.api.UpdateLecturerPreferenceResponse
 import com.kul.database.lecturerpreferences.domain.LecturerPreferences;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class UpdateLecturerPreferenceMapper {
     public static UpdateLecturerPreferenceResponse fromDomain(LecturerPreferences lecturerPreferences) {
         return new UpdateLecturerPreferenceResponse(
                 lecturerPreferences.getId(),
                 lecturerPreferences.getUser().getId(),
-                localTimeToHourAndMinute(lecturerPreferences.getStartTime()),
-                localTimeToHourAndMinute(lecturerPreferences.getEndTime()),
+                localTimeToString(lecturerPreferences.getStartTime()),
+                localTimeToString(lecturerPreferences.getEndTime()),
                 lecturerPreferences.getDay().name()
         );
     }
 
-    private static String localTimeToHourAndMinute(LocalTime time) {
-        return time.getHour() + ":" + time.getMinute();
+    private static String localTimeToString(LocalTime time) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+        return formatter.format(time);
     }
 }
